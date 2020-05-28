76c69b48a57bff6b05563586eea9dcf20ebeba6e
Norcelo De Miranda <norcelodemiranda@gmail.com>
2020-05-28 18:57:42 -0300

    Closes #2. Resta de dos enteros.

---
+++ resta.c
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+main()
+    {int a,b,resta;
+        //Muestra mensaje por pantalla
+        cout<<"\n Ingrese un numero: ";
+        //Lee un número
+        cin>>a;
+        //Muestra mensaje por pantall
+        cout<<"\n Ingrese un numero: ";
+        //Lee un número
+        cin>>b;
+        //El entero resta, almacena el resultado de restar a-b
+        resta=a-b;
+        //Muestra el resultado
+        cout<<"\n La resta de "<<a<<"+"<<b<<" es: "<<resta;
+        return 0;
+    }
