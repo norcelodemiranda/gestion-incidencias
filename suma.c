f19aff54152efa4a5fde728388df320381e88fb8
Norcelo De Miranda <norcelodemiranda@gmail.com>
2020-05-28 18:50:54 -0300

    Closes #1. Suma de dos enteros.

---
+++ suma.c
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+main()
+    {int a,b,suma;
+        //Muestra mensaje por pantalla
+        cout<<"\n Ingrese un numero: ";
+        //Lee un número
+        cin>>a;
+        //Muestra mensaje por pantall
+        cout<<"\n Ingrese un numero: ";
+        //Lee un número
+        cin>>b;
+        //El entero suma, almacena el resultado de sumar a+b
+        suma=a+b;
+        //Muestra el resultado
+        cout<<"\n La suma de "<<a<<"+"<<b<<" es: "<<suma;
+        return 0;
+    }
