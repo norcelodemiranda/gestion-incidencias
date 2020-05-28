dee4bafe0d3c3c9e5431945082e98ed9a4fe5834
Leandro Diaz <norcelo21@gmail.com>
2020-05-28 19:22:59 -0300

    Closes #3. Multiplicación de dos enteros.

---
+++ multiplicacion.c
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+main()
+    {int a,b,mult;
+        //Muestra mensaje por pantalla
+        cout<<"\n Ingrese un numero: ";
+        //Lee un número
+        cin>>a;
+        //Muestra mensaje por pantalla
+        cout<<"\n Ingrese un numero: ";
+        //Lee un número
+        cin>>b;
+        //El entero mult, almacena el resultado de multiplicar a*b
+        mult=a*b;
+        //Muestra el resultado
+        cout<<"\n El resultado de la multiplicacion "<<a<<"*"<<b<<" es: "<<mult;
+        return 0;
+    }
